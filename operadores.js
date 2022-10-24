d20a89399e1ace0ec83d889986ec42a7546744ae
Amor Dirazar <dirazarmariaamor@gmail.com>
2022-10-24 10:20:50 -0300

    ejercicios lunes 24

---
+++ operadores.js
@@ -0,0 +1,17 @@
+console.log ( 2 + 3 )
+
+console.log ( 3 - 8 )
+
+console.log ( 16 / 4 )
+
+console.log ( 3 * 4 )
+
+console.log ( 7 % 2 )
+
+let numero = 3
+console.log (++numero)
+console.log (--numero)
+
+console.log ( 5 < 4 )
+console.log ( 5!=="5")
+
--- probando.js
+++ probando.js
@@ -14,3 +14,30 @@ if (true){
     let nombre = "Benja"
     console.log(nombre)
 }
+let edad = 25
+console.log(edad)
+let precio = 12.5
+console.log(precio)
+let HolaMundo = "Hola Mundo"
+console.log (HolaMundo)
+let gatos = true
+console.log(gatos)
+let perros = false
+console.log(perros)
+let NotANumber = NaN
+console.log(NotANumber)
+let nulo = null
+console.log(nulo)
+let casa;
+console.log(casa)
+let computadora =
+{
+    marca: "Lenovo",
+    modelo: 2022,
+    estadoEncendida: true
+}
+console.log(computadora)
+let comida = [
+    "Hamburguesas", "Pure", "Milanesas"
+]
+console.log(comida)
