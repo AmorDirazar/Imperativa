aac15bf5facbe9f4d2d52aed3f2aa2cd960a6212
Amor Dirazar <dirazarmariaamor@gmail.com>
2022-10-23 23:39:25 -0300

    jugando con js

--- probando.js
+++ probando.js
@@ -2,4 +2,15 @@ let nombre = "Amor"
 
 console.log(nombre)
 
-console.log("Hola Mundo!")
+if (true){
+    let nombre = "Cleo"
+    console.log(nombre)
+    if (true){
+        let nombre = "Andrea"
+        console.log(nombre)
+    }
+}
+if (true){
+    let nombre = "Benja"
+    console.log(nombre)
+}
